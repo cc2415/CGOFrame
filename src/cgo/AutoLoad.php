668bad2624b273f609d929aa23ef0987e221646d
cheng <137842934@qq.com>
2022-01-12 14:50:14 +0800

    init

--- src/cgo/AutoLoad.php
+++ src/cgo/AutoLoad.php
@@ -6,8 +6,7 @@ class AutoLoad
     public static function load($className)
     {
         echo $className . PHP_EOL;
-        $fileName = str_replace('\\','/', DIR.'\\'. $className).'.php';
-        echo $fileName . PHP_EOL;
+        $fileName = str_replace('\\','/', DIR.'\\..\\'. $className).'.php';
         if(is_file($fileName)){
             require $fileName;
         }else{
